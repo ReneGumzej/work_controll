a3c74231057de3a3bafbe5a3ac39fcad8af975cc
ReneGumzej <82899981+ReneGumzej@users.noreply.github.com>
2023-01-23 15:11:09 +0100

    Update __init__.py

--- work_controll/__init__.py
+++ work_controll/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from secrets import token_hex
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"]="" #Secret key ist für Schutz gegen CSRF
+app.config["SECRET_KEY"]="token_hex(16)" #Secret key ist für Schutz gegen CSRF
 app.config['SQLALCHEMY_DATABASE_URI'] = ""
 app.config['SQLALCHEMY_TRACK_MODIFIKATONS'] = False
 db = SQLAlchemy(app)
@@ -15,5 +16,5 @@ login_manager.login_view = 'login'
 login_manager.login_message = u"Bitte Anmelden"
 login_manager.login_message_category = 'info'
 
-from . import routes, models #relativer Pfad. "." = "work_controll" 
+from . import routes, models
 
